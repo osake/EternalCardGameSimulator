845e298eee979edfbf2af55fbb994e9d70711806
Joshua French <osakenoyojimbo@gmail.com>
2017-01-29 18:33:10 -0700

    Cleanup and rename deck

--- src/main/scala/example/Deck.scala
+++ src/main/scala/example/Deck.scala
@@ -6,22 +6,20 @@ import scala.collection.mutable.ListBuffer
 /**
   * Deck object which holds a list of cards.
   */
-class Deck(val deck: ListBuffer[Card]) {
-
-  var d: ListBuffer[Card] = deck
+class Deck(var deck: ListBuffer[Card]) {
 
   // Convenience method for removing a card.
   def draw() : Card = {
-    return d.remove(0)
+    return deck.remove(0)
   }
 
   // Convenience method for putting back a card.
   def replace(card: Card) : Unit = {
-    d += card
+    deck += card
   }
 
   // Utilize Scala's shuffle method to shuffle the deck.
   def shuffle() {
-    d = Random.shuffle(d)
+    deck = Random.shuffle(d)
   }
 }
